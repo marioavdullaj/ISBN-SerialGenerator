8248c00436116b95f4511489380c2dc53ccec788
armand ferati <armandferati@hotmail.com>
2020-08-19 15:38:16 +0300

    Changes progressbar by (MandisPro)

--- DocumentSerials/DocumentSerials/PasswordManager.Designer.cs
+++ DocumentSerials/DocumentSerials/PasswordManager.Designer.cs
@@ -37,6 +37,9 @@
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.panel5 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtTimer = new System.Windows.Forms.TextBox();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.exportButton = new System.Windows.Forms.Button();
             this.generateButton = new System.Windows.Forms.Button();
@@ -76,9 +79,9 @@
             this.panel1.Controls.Add(this.label1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(698, 60);
+            this.panel1.Size = new System.Drawing.Size(524, 49);
             this.panel1.TabIndex = 2;
             // 
             // label1
@@ -86,9 +89,10 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.Control;
-            this.label1.Location = new System.Drawing.Point(155, 9);
+            this.label1.Location = new System.Drawing.Point(116, 7);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(412, 39);
+            this.label1.Size = new System.Drawing.Size(329, 31);
             this.label1.TabIndex = 0;
             this.label1.Text = "Universal Code Generator";
             // 
@@ -96,10 +100,10 @@
             // 
             this.panel2.Controls.Add(this.tabControl1);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel2.Location = new System.Drawing.Point(0, 60);
-            this.panel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel2.Location = new System.Drawing.Point(0, 49);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(698, 675);
+            this.panel2.Size = new System.Drawing.Size(524, 548);
             this.panel2.TabIndex = 3;
             // 
             // tabControl1
@@ -107,10 +111,10 @@
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tabControl1.Location = new System.Drawing.Point(0, 0);
-            this.tabControl1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(2);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(698, 675);
+            this.tabControl1.Size = new System.Drawing.Size(524, 548);
             this.tabControl1.TabIndex = 0;
             // 
             // tabPage1
@@ -118,11 +122,11 @@
             this.tabPage1.Controls.Add(this.panel5);
             this.tabPage1.Controls.Add(this.panel4);
             this.tabPage1.Controls.Add(this.panel3);
-            this.tabPage1.Location = new System.Drawing.Point(4, 25);
-            this.tabPage1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(2);
             this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.tabPage1.Size = new System.Drawing.Size(690, 646);
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(2);
+            this.tabPage1.Size = new System.Drawing.Size(516, 522);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Generate";
             this.tabPage1.UseVisualStyleBackColor = true;
@@ -130,29 +134,58 @@
             // panel5
             // 
             this.panel5.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.panel5.Controls.Add(this.button2);
+            this.panel5.Controls.Add(this.label4);
+            this.panel5.Controls.Add(this.txtTimer);
             this.panel5.Controls.Add(this.progressBar1);
             this.panel5.Controls.Add(this.exportButton);
             this.panel5.Controls.Add(this.generateButton);
             this.panel5.Controls.Add(this.button1);
             this.panel5.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel5.Location = new System.Drawing.Point(3, 546);
+            this.panel5.Location = new System.Drawing.Point(2, 444);
+            this.panel5.Margin = new System.Windows.Forms.Padding(2);
             this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(684, 98);
+            this.panel5.Size = new System.Drawing.Size(512, 76);
             this.panel5.TabIndex = 7;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(196, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 36);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Cancel";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(345, 7);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 10;
+            // 
+            // txtTimer
+            // 
+            this.txtTimer.Location = new System.Drawing.Point(418, 4);
+            this.txtTimer.Name = "txtTimer";
+            this.txtTimer.Size = new System.Drawing.Size(88, 20);
+            this.txtTimer.TabIndex = 9;
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(0, 5);
+            this.progressBar1.Location = new System.Drawing.Point(0, 4);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(2);
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(684, 24);
+            this.progressBar1.Size = new System.Drawing.Size(328, 20);
             this.progressBar1.TabIndex = 9;
             // 
             // exportButton
             // 
-            this.exportButton.Location = new System.Drawing.Point(130, 49);
-            this.exportButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.exportButton.Location = new System.Drawing.Point(98, 40);
+            this.exportButton.Margin = new System.Windows.Forms.Padding(2);
             this.exportButton.Name = "exportButton";
-            this.exportButton.Size = new System.Drawing.Size(124, 43);
+            this.exportButton.Size = new System.Drawing.Size(93, 35);
             this.exportButton.TabIndex = 5;
             this.exportButton.Text = "Export";
             this.exportButton.UseVisualStyleBackColor = true;
@@ -160,10 +193,10 @@
             // 
             // generateButton
             // 
-            this.generateButton.Location = new System.Drawing.Point(0, 49);
-            this.generateButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.generateButton.Location = new System.Drawing.Point(0, 40);
+            this.generateButton.Margin = new System.Windows.Forms.Padding(2);
             this.generateButton.Name = "generateButton";
-            this.generateButton.Size = new System.Drawing.Size(124, 42);
+            this.generateButton.Size = new System.Drawing.Size(93, 34);
             this.generateButton.TabIndex = 3;
             this.generateButton.Text = "Generate";
             this.generateButton.UseVisualStyleBackColor = true;
@@ -171,10 +204,10 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(557, 49);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Location = new System.Drawing.Point(418, 40);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(127, 43);
+            this.button1.Size = new System.Drawing.Size(95, 35);
             this.button1.TabIndex = 6;
             this.button1.Text = "Clear All";
             this.button1.UseVisualStyleBackColor = true;
@@ -184,9 +217,10 @@
             // 
             this.panel4.Controls.Add(this.dataGridView1);
             this.panel4.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel4.Location = new System.Drawing.Point(3, 145);
+            this.panel4.Location = new System.Drawing.Point(2, 118);
+            this.panel4.Margin = new System.Windows.Forms.Padding(2);
             this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(684, 401);
+            this.panel4.Size = new System.Drawing.Size(512, 326);
             this.panel4.TabIndex = 6;
             // 
             // dataGridView1
@@ -203,12 +237,12 @@
             this.Password});
             this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dataGridView1.Location = new System.Drawing.Point(0, 0);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowHeadersWidth = 62;
             this.dataGridView1.RowTemplate.Height = 28;
-            this.dataGridView1.Size = new System.Drawing.Size(684, 401);
+            this.dataGridView1.Size = new System.Drawing.Size(512, 326);
             this.dataGridView1.TabIndex = 0;
             // 
             // N
@@ -227,7 +261,6 @@
             this.Document.MinimumWidth = 8;
             this.Document.Name = "Document";
             this.Document.ReadOnly = true;
-            this.Document.Width = 200;
             // 
             // Password
             // 
@@ -249,74 +282,77 @@
             this.panel3.Controls.Add(this.textBox2);
             this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel3.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.panel3.Location = new System.Drawing.Point(3, 2);
-            this.panel3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel3.Location = new System.Drawing.Point(2, 2);
+            this.panel3.Margin = new System.Windows.Forms.Padding(2);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(684, 143);
+            this.panel3.Size = new System.Drawing.Size(512, 116);
             this.panel3.TabIndex = 5;
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(311, 39);
-            this.comboBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.comboBox1.Location = new System.Drawing.Point(233, 32);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(368, 24);
+            this.comboBox1.Size = new System.Drawing.Size(277, 21);
             this.comboBox1.TabIndex = 8;
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(4, 42);
+            this.label7.Location = new System.Drawing.Point(3, 34);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(113, 17);
+            this.label7.Size = new System.Drawing.Size(85, 13);
             this.label7.TabIndex = 7;
             this.label7.Text = "License duration";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(518, 112);
+            this.label3.Location = new System.Drawing.Point(388, 91);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(66, 17);
+            this.label3.Size = new System.Drawing.Size(51, 13);
             this.label3.TabIndex = 4;
             this.label3.Text = "N. Codes";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(3, 14);
+            this.label2.Location = new System.Drawing.Point(2, 11);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(39, 17);
+            this.label2.Size = new System.Drawing.Size(32, 13);
             this.label2.TabIndex = 0;
             this.label2.Text = "ISBN";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(590, 109);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox1.Location = new System.Drawing.Point(442, 89);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(89, 22);
+            this.textBox1.Size = new System.Drawing.Size(68, 20);
             this.textBox1.TabIndex = 1;
             this.textBox1.Text = "0";
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(311, 9);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox2.Location = new System.Drawing.Point(233, 7);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(368, 22);
+            this.textBox2.Size = new System.Drawing.Size(277, 20);
             this.textBox2.TabIndex = 2;
             // 
             // PasswordManager
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(698, 735);
+            this.ClientSize = new System.Drawing.Size(524, 597);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.MaximizeBox = false;
             this.Name = "PasswordManager";
             this.RightToLeft = System.Windows.Forms.RightToLeft.No;
@@ -328,6 +364,7 @@
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
             this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
             this.panel4.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.panel3.ResumeLayout(false);
@@ -362,6 +399,9 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn N;
         private System.Windows.Forms.DataGridViewTextBoxColumn Document;
         private System.Windows.Forms.DataGridViewTextBoxColumn Password;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtTimer;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- DocumentSerials/DocumentSerials/PasswordManager.cs
+++ DocumentSerials/DocumentSerials/PasswordManager.cs
@@ -9,45 +9,40 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
 
 namespace DocumentSerials
 {
     public partial class PasswordManager : Form
     {
         private SerialCode sc;
+
         public PasswordManager()
         {
+            
             Init();
-        }// so finally, when you modify, you need to do this
-        // hereherehererer 00.20
-        //do it , try
-        //ok
-        // no, first save, then commit, then commit changes, then sync
-        // you didn't tell me that, i always save with CTRL+S so you didnt see it :D+
-        //mod 00.25 by mario//ok
-        // now we can work in the same project, i can see you changes, you can see mine, and sync
-        //cool
-        // if i upload a project i do the same?
-        // no you need to create a new repository on github, clone it like you've seen before, and then it's the same
-        // but first in github
+        }
         private void Init()
         {
             InitializeComponent();
             sc = new SerialCode();
-            
+           
             // initialize combobox
-            for(int i = 1; i <= 36; i++)
+            for (int i = 1; i <= 36; i++)
             {
                 comboBox1.Items.Add(i + " Months");
             }
             comboBox1.SelectedIndex = 0;
+            
+            
         }
 
 
         private void generatePasswords(string doc, int duration, int n)
         {
+           
             //List<string> passwords = new List<string> { };
-
             progressBar1.Value = 0;
             progressBar1.Maximum = n;
             string psw;
@@ -64,10 +59,11 @@ namespace DocumentSerials
                 dt = (DataTable)dataGridView1.DataSource;
 
             dataGridView1.UseWaitCursor = true;
-
+            
             for (int actual_rows = dt.Rows.Count, i = actual_rows + 1; i <= actual_rows + n; i++)
             {
-                psw = sc.Generate(doc, duration);
+                
+                    psw = sc.Generate(doc, duration);
                 DataRow dr = dt.NewRow();
                 dr[0] = i.ToString();
                 dr[1] = doc;
@@ -77,12 +73,45 @@ namespace DocumentSerials
             }
             dataGridView1.DataSource = dt;
             dataGridView1.UseWaitCursor = false;
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            stopWatch.Stop();
+
+            
+            
+                TimeSpan ts = stopWatch.Elapsed;
+                txtTimer.Text = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                      ts.Hours, ts.Minutes, ts.Seconds,
+                      ts.Milliseconds / 10);
+                Thread.Sleep(0);
+
+                progressBar1.Value = 0;
+                label4.Text = Convert.ToString((int)((float)0 / (float)n * 100)) + "%";
+            
+
+            
+            
 
             return;
 
+
+
+
+
+
+
+
+
         }
+
         public void button1_Click(object sender, EventArgs e)
         {
+            
+
+        
+            
+            
+            
 
             string documentNumber = textBox2.Text;
             int duration = comboBox1.SelectedIndex + 1;
@@ -104,13 +133,18 @@ namespace DocumentSerials
                     return;
                 }
             }
-            catch(Exception _)
+            catch (Exception)
             {
                 MessageBox.Show("The number of passwords format is not valid, please insert a number");
             }
 
             generatePasswords(documentNumber, duration, numberOfPasswords);
+
+            
+
+
         }
+
 
         private void exportButton_Click(object sender, EventArgs e)
         {
@@ -119,8 +153,8 @@ namespace DocumentSerials
                 dialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
                 dialog.FilterIndex = 2;
                 dialog.RestoreDirectory = true;
-                dialog.FileName = "SerialCodes_" + textBox2.Text + 
-                    "-" + DateTime.Today.Day + "-" + DateTime.Today.Month + "-" + DateTime.Today.Year + 
+                dialog.FileName = "SerialCodes_" + textBox2.Text +
+                    "-" + DateTime.Today.Day + "-" + DateTime.Today.Month + "-" + DateTime.Today.Year +
                     ".txt";
 
                 if (dialog.ShowDialog() == DialogResult.OK)
@@ -129,7 +163,8 @@ namespace DocumentSerials
                     using (Stream stream = dialog.OpenFile())
                     {
                         DataTable dt = (DataTable)dataGridView1.DataSource;
-                        foreach(DataRow row in dt.Rows) {
+                        foreach (DataRow row in dt.Rows)
+                        {
                             byte[] output = Encoding.UTF8.GetBytes(row[2].ToString() + "\n");
                             stream.Write(output, 0, output.Length);
                         }
@@ -142,9 +177,21 @@ namespace DocumentSerials
         private void button1_Click_1(object sender, EventArgs e)
         {
             DataTable dt = (DataTable)dataGridView1.DataSource;
-            if(dt != null)
+            if (dt != null)
                 dt.Clear();
             dataGridView1.DataSource = dt;
         }
+
     }
 }
+    
+       
+
+       
+
+        
+
+    
+
+
+
