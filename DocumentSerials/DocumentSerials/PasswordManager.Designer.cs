9b21bfd250cdf33e8f199a972a171c6871690428
marioavdullaj <smb995@gmail.com>
2020-08-20 12:47:42 +0200

    Connected label

--- DocumentSerials/DocumentSerials/PasswordManager.Designer.cs
+++ DocumentSerials/DocumentSerials/PasswordManager.Designer.cs
@@ -49,8 +49,6 @@
             this.Password = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Duration = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.panel3 = new System.Windows.Forms.Panel();
-            this.countTextBox = new System.Windows.Forms.TextBox();
-            this.bookComboBox = new System.Windows.Forms.ComboBox();
             this.numBlockTextBox = new System.Windows.Forms.TextBox();
             this.codeSizeTextBox = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
@@ -60,8 +58,9 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.bookComboBox = new System.Windows.Forms.ComboBox();
             this.label6 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
+            this.countTextBox = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
@@ -82,12 +81,10 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.panel1.Controls.Add(this.label8);
-            this.panel1.Controls.Add(this.label6);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(699, 60);
             this.panel1.TabIndex = 2;
@@ -97,7 +94,8 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.Control;
-            this.label1.Location = new System.Drawing.Point(6, 11);
+            this.label1.Location = new System.Drawing.Point(116, 7);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(412, 39);
             this.label1.TabIndex = 0;
@@ -107,8 +105,8 @@
             // 
             this.panel2.Controls.Add(this.tabControl1);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel2.Location = new System.Drawing.Point(0, 60);
-            this.panel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel2.Location = new System.Drawing.Point(0, 49);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(699, 675);
             this.panel2.TabIndex = 3;
@@ -118,7 +116,7 @@
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tabControl1.Location = new System.Drawing.Point(0, 0);
-            this.tabControl1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
             this.tabControl1.Size = new System.Drawing.Size(699, 675);
@@ -129,11 +127,11 @@
             this.tabPage1.Controls.Add(this.panel5);
             this.tabPage1.Controls.Add(this.panel4);
             this.tabPage1.Controls.Add(this.panel3);
-            this.tabPage1.Location = new System.Drawing.Point(4, 25);
-            this.tabPage1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.tabPage1.Size = new System.Drawing.Size(691, 646);
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage1.Size = new System.Drawing.Size(516, 522);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Generate";
             this.tabPage1.UseVisualStyleBackColor = true;
@@ -147,8 +145,8 @@
             this.panel5.Controls.Add(this.generateButton);
             this.panel5.Controls.Add(this.clearAllButton);
             this.panel5.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel5.Location = new System.Drawing.Point(3, 546);
-            this.panel5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel5.Location = new System.Drawing.Point(2, 444);
+            this.panel5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel5.Name = "panel5";
             this.panel5.Size = new System.Drawing.Size(685, 98);
             this.panel5.TabIndex = 7;
@@ -163,16 +161,16 @@
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(1, 5);
-            this.progressBar1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.progressBar1.Location = new System.Drawing.Point(1, 4);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.progressBar1.Name = "progressBar1";
             this.progressBar1.Size = new System.Drawing.Size(581, 25);
             this.progressBar1.TabIndex = 9;
             // 
             // exportButton
             // 
-            this.exportButton.Location = new System.Drawing.Point(427, 49);
-            this.exportButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.exportButton.Location = new System.Drawing.Point(320, 40);
+            this.exportButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.exportButton.Name = "exportButton";
             this.exportButton.Size = new System.Drawing.Size(124, 43);
             this.exportButton.TabIndex = 5;
@@ -182,8 +180,8 @@
             // 
             // generateButton
             // 
-            this.generateButton.Location = new System.Drawing.Point(0, 49);
-            this.generateButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.generateButton.Location = new System.Drawing.Point(0, 40);
+            this.generateButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.generateButton.Name = "generateButton";
             this.generateButton.Size = new System.Drawing.Size(124, 42);
             this.generateButton.TabIndex = 3;
@@ -193,8 +191,8 @@
             // 
             // clearAllButton
             // 
-            this.clearAllButton.Location = new System.Drawing.Point(557, 49);
-            this.clearAllButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.clearAllButton.Location = new System.Drawing.Point(418, 40);
+            this.clearAllButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.clearAllButton.Name = "clearAllButton";
             this.clearAllButton.Size = new System.Drawing.Size(127, 43);
             this.clearAllButton.TabIndex = 6;
@@ -206,8 +204,8 @@
             // 
             this.panel4.Controls.Add(this.dataGridView1);
             this.panel4.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel4.Location = new System.Drawing.Point(3, 145);
-            this.panel4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel4.Location = new System.Drawing.Point(2, 118);
+            this.panel4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel4.Name = "panel4";
             this.panel4.Size = new System.Drawing.Size(685, 401);
             this.panel4.TabIndex = 6;
@@ -227,7 +225,7 @@
             this.Duration});
             this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dataGridView1.Location = new System.Drawing.Point(0, 0);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowHeadersWidth = 62;
@@ -287,35 +285,16 @@
             this.panel3.Controls.Add(this.textBox1);
             this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel3.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.panel3.Location = new System.Drawing.Point(3, 2);
-            this.panel3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel3.Location = new System.Drawing.Point(2, 2);
+            this.panel3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel3.Name = "panel3";
             this.panel3.Size = new System.Drawing.Size(685, 143);
             this.panel3.TabIndex = 5;
             // 
-            // countTextBox
-            // 
-            this.countTextBox.Enabled = false;
-            this.countTextBox.Location = new System.Drawing.Point(442, 11);
-            this.countTextBox.Margin = new System.Windows.Forms.Padding(4);
-            this.countTextBox.Name = "countTextBox";
-            this.countTextBox.Size = new System.Drawing.Size(235, 22);
-            this.countTextBox.TabIndex = 15;
-            // 
-            // bookComboBox
-            // 
-            this.bookComboBox.FormattingEnabled = true;
-            this.bookComboBox.Location = new System.Drawing.Point(131, 10);
-            this.bookComboBox.Margin = new System.Windows.Forms.Padding(4);
-            this.bookComboBox.Name = "bookComboBox";
-            this.bookComboBox.Size = new System.Drawing.Size(303, 24);
-            this.bookComboBox.TabIndex = 13;
-            this.bookComboBox.SelectedIndexChanged += new System.EventHandler(this.bookComboBox_SelectedIndexChanged);
-            // 
             // numBlockTextBox
             // 
-            this.numBlockTextBox.Location = new System.Drawing.Point(569, 75);
-            this.numBlockTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.numBlockTextBox.Location = new System.Drawing.Point(445, 64);
+            this.numBlockTextBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.numBlockTextBox.Name = "numBlockTextBox";
             this.numBlockTextBox.Size = new System.Drawing.Size(107, 22);
             this.numBlockTextBox.TabIndex = 12;
@@ -323,8 +302,8 @@
             // 
             // codeSizeTextBox
             // 
-            this.codeSizeTextBox.Location = new System.Drawing.Point(569, 43);
-            this.codeSizeTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.codeSizeTextBox.Location = new System.Drawing.Point(445, 38);
+            this.codeSizeTextBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.codeSizeTextBox.Name = "codeSizeTextBox";
             this.codeSizeTextBox.Size = new System.Drawing.Size(107, 22);
             this.codeSizeTextBox.TabIndex = 11;
@@ -351,8 +330,8 @@
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(131, 42);
-            this.comboBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.comboBox1.Location = new System.Drawing.Point(98, 34);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(302, 24);
             this.comboBox1.TabIndex = 8;
@@ -386,32 +365,38 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(569, 107);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox1.Location = new System.Drawing.Point(444, 92);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(108, 22);
             this.textBox1.TabIndex = 1;
             this.textBox1.Text = "0";
             // 
+            // bookComboBox
+            // 
+            this.bookComboBox.FormattingEnabled = true;
+            this.bookComboBox.Location = new System.Drawing.Point(98, 8);
+            this.bookComboBox.Name = "bookComboBox";
+            this.bookComboBox.Size = new System.Drawing.Size(172, 21);
+            this.bookComboBox.TabIndex = 13;
+            this.bookComboBox.SelectedIndexChanged += new System.EventHandler(this.bookComboBox_SelectedIndexChanged);
+            // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label6.Location = new System.Drawing.Point(539, 9);
+            this.label6.Location = new System.Drawing.Point(365, 11);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(125, 17);
-            this.label6.TabIndex = 1;
-            this.label6.Text = "Connection status:";
+            this.label6.Size = new System.Drawing.Size(97, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "# codes generated";
             // 
-            // label8
+            // countTextBox
             // 
-            this.label8.AutoSize = true;
-            this.label8.ForeColor = System.Drawing.Color.DarkRed;
-            this.label8.Location = new System.Drawing.Point(552, 33);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(100, 17);
-            this.label8.TabIndex = 2;
-            this.label8.Text = "Not connected";
+            this.countTextBox.Enabled = false;
+            this.countTextBox.Location = new System.Drawing.Point(276, 8);
+            this.countTextBox.Name = "countTextBox";
+            this.countTextBox.Size = new System.Drawing.Size(83, 20);
+            this.countTextBox.TabIndex = 15;
             // 
             // PasswordManager
             // 
@@ -421,7 +406,7 @@
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
             this.Name = "PasswordManager";
             this.RightToLeft = System.Windows.Forms.RightToLeft.No;
@@ -478,6 +463,7 @@
         private System.Windows.Forms.TextBox countTextBox;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label8;
     }
 }
 
