ef95c1e0adae1c86e20b5d7e45cc328786d38237
marioavdullaj <smb995@gmail.com>
2020-08-20 14:34:33 +0200

    ..

--- DocumentSerials/DocumentSerials/PasswordManager.Designer.cs
+++ DocumentSerials/DocumentSerials/PasswordManager.Designer.cs
@@ -143,7 +143,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.label1.Location = new System.Drawing.Point(63, 9);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
