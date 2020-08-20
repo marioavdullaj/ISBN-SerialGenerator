ca5886900df330350ea0d6815de7e45b4b0ecb9c
marioavdullaj <smb995@gmail.com>
2020-08-20 11:47:09 +0200

    New hashing algorithm

--- DocumentSerials/DocumentSerials/Utils.cs
+++ DocumentSerials/DocumentSerials/Utils.cs
@@ -3,21 +3,23 @@ using System.Collections.Generic;
 using System.Text;
 using System.Security.Cryptography;
 using System.Windows.Forms;
+using System.Security.Policy;
 
 namespace DocumentSerials
 {
     public class SerialCode
     {
-        private List<string> documentCodes = new List<string> { };
+        private List<string> documentCodes;
+        private Random _random;
 
         private const char groupSeparator = '-';
-        private const string chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         public int NumBlocks { get; set; }
         public int Size { get; set; }
 
         public SerialCode()
         {
-            Console.WriteLine("Creating a new SerialCode object");
+            documentCodes = new List<string>();
+            _random = new Random();
             // Setting default values
             NumBlocks = 4;
             Size = 20;
@@ -32,21 +34,58 @@ namespace DocumentSerials
             return;
         }
 
-        private string HashString(string text)
+        private T[] Shuffle<T>(T[] array)
         {
+            int n = array.Length;
+            for (int i = 0; i < n; i++)
+            {
+                int r = i + _random.Next(n - i);
+                T t = array[r];
+                array[r] = array[i];
+                array[i] = t;
+            }
+            return array;
+        }
+        // OLD DEPRECATED ALGORITHM
+        private string SHA256DownsampledHash(string text)
+        {
+            string chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
             byte[] bytes = Encoding.UTF8.GetBytes(text);
 
             SHA256Managed hashstring = new SHA256Managed();
             byte[] hash = hashstring.ComputeHash(bytes);
-
             char[] hash2 = new char[Size];
 
             for (int i = 0; i < hash2.Length; i++)
             {
                 hash2[i] = chars[hash[i] % chars.Length];
             }
-
             return new string(hash2);
+        }
+        /*
+         * This new algorithm merges and shuffles randomly two hashes of the same code text, and returns
+         * a substring with the specified length
+         */
+        private string HashingAlgorithm(string text)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(text);
+
+            SHA256Managed sha256 = new SHA256Managed();
+            MD5 md5 = MD5.Create();
+
+            byte[] hash1 = sha256.ComputeHash(bytes);
+            byte[] hash2 = md5.ComputeHash(bytes);
+
+            StringBuilder hash1String = new StringBuilder();
+            StringBuilder hash2String = new StringBuilder();
+            // Convert the hash byte arrays into hexadecimal strings
+            for (int i = 0; i < hash1.Length; i++)
+                hash1String.Append(hash1[i].ToString("X2"));
+            for (int i = 0; i < hash2.Length; i++)
+                hash2String.Append(hash2[i].ToString("X2"));
+            // Random shuffle of the concatenation of the two hashes
+            byte[] code = Shuffle(Encoding.UTF8.GetBytes(hash1String.ToString() + hash2String.ToString()));
+            return Encoding.UTF8.GetString(code).Substring(0, Size);
         }
 
         public string Generate(string document, int duration, int sequenceNumber)
@@ -55,7 +94,7 @@ namespace DocumentSerials
 
             string text = document + groupSeparator + duration + groupSeparator + Convert.ToString(sequenceNumber);
 
-            string code = this.HashString(text);
+            string code = this.HashingAlgorithm(text);
             string groupedCode = "";
             for(int i = 0, block_size = Convert.ToInt32(code.Length / NumBlocks); i < code.Length; i += block_size)
             {
